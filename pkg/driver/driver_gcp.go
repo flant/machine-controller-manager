0b4873ad23c9c9c708b2c38211c52c396b301647
Andrey Klimentyev <andrey.klimentyev@flant.com>
2019-09-16 17:56:16 +0300

    Remove emptyAccessConfig for now
    Signed-off-by: Andrey Klimentyev <andrey.klimentyev@flant.com>

--- pkg/driver/driver_gcp.go
+++ pkg/driver/driver_gcp.go
@@ -112,9 +112,7 @@ func (d *GCPDriver) Create() (string, string, error) {
 
 	var networkInterfaces = []*compute.NetworkInterface{}
 	for _, nic := range d.GCPMachineClass.Spec.NetworkInterfaces {
-		computeNIC := &compute.NetworkInterface{
-			AccessConfigs: []*compute.AccessConfig{{}},
-		}
+		computeNIC := &compute.NetworkInterface{}
 		if len(nic.Network) != 0 {
 			computeNIC.Network = fmt.Sprintf("projects/%s/global/networks/%s", project, nic.Network)
 		}
